6851d9095e322095458f17145774c60e7b985d11
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-09-11 20:19:24 +0530

    11/9

--- shtest/nodeman.js
+++ shtest/nodeman.js
@@ -2,7 +2,8 @@ const http = require('http');
 const { exec } = require('child_process');
 
 http.createServer((request, response) => {
-exec("sh test.sh")
+    // exec("sh test.sh")
+    response.end("works")
 }).listen(6006);
 
 console.log('Server running at http://localhost:6006/');
