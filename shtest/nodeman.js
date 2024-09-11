ea67949169012d60f029f54f1a7c2f705ce543c3
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-09-11 21:04:00 +0530

    11/9

--- shtest/nodeman.js
+++ shtest/nodeman.js
@@ -6,14 +6,27 @@ http.createServer((request, response) => {
     const substr = '/mylibg';
 
     if (path.includes(substr)) {
-        console.log('server called');
-        exec("sh test.sh")
-        response.end("Unauthorized")
+        console.log('Server called');
+        
+        // Execute shell script
+        exec("sh test.sh", (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                response.statusCode = 500;
+                response.end(`Server error: ${error.message}`);
+                return;
+            }
 
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+            response.end("Shell script executed");
+        });
 
     } else {
         response.statusCode = 404;
         response.end("Not Found");
     } 
 
-}).listen(6004);
+}).listen(6004, () => {
+    console.log('Server running at http://localhost:6004/');
+});
