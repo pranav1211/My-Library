2103716f2c4d9d460fe8dd1073bc087034d3a2d8
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-09-11 20:43:48 +0530

    11/9

--- shtest/nodeman.js
+++ shtest/nodeman.js
@@ -1,7 +1,18 @@
 const http = require('http');
 
 http.createServer((request, response) => {
-    response.end("works")
-}).listen(6004);
+    const path = request.url;
+    const substr = '/mylibgit';
 
+    if (path.includes(substr)) {
+        console.log('server called');
+        response.end("Unauthorized")
+
+
+    } else {
+        response.statusCode = 404;
+        response.end("Not Found");
+    }
+
+}).listen(6003);
 console.log('Server running at http://localhost:6006/');
