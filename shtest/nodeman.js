3846f9c8df04ad93a6a312a5341676f8cb1309d0
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-09-11 19:58:19 +0530

    11/9

--- shtest/nodeman.js
+++ shtest/nodeman.js
@@ -2,7 +2,20 @@ const http = require('http');
 const { exec } = require('child_process');
 
 http.createServer((request, response) => {
-    exec('test.sh')
+    exec('sh test.sh', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing script: ${error}`);
+            response.writeHead(500, { 'Content-Type': 'text/plain' });
+            response.end('Error executing script.');
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+        }
+        console.log(`stdout: ${stdout}`);
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end('Script executed successfully.');
+    });
 }).listen(6006);
 
 console.log('Server running at http://localhost:6006/');
