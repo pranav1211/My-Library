29394c3a075d39e3130d9f70b8b594a4b349c19b
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-09-11 20:54:08 +0530

    11/9

--- shtest/nodeman.js
+++ shtest/nodeman.js
@@ -1,17 +1,7 @@
+const { exec } = require('child_process');
 const http = require('http');
 
 http.createServer((request, response) => {
-    const path = request.url;
-    const substr = '/mylibg';
-
-    if (path.includes(substr)) {
-        console.log('server called');
-        response.end("Unauthorized")
-
-
-    } else {
-        response.statusCode = 404;
-        response.end("Not Found");
-    }
+ exec("sh test.sh")
 
 }).listen(6004);
