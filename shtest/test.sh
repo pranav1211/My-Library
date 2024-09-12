7033c732c314c55ccf486c1ae7736b4dc160d65b
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-09-12 12:25:18 +0530

    12/9

--- shtest/test.sh
+++ shtest/test.sh
@@ -1,1 +1,12 @@
-echo "Shell script is running"
+#!/bin/bash
+
+# Define the log file path
+LOG_FILE="/mylib/shtest/test.log"
+
+# Get the current date and time
+CURRENT_TIME=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Write a log entry
+echo "Script called at $CURRENT_TIME" >> "$LOG_FILE"
+
+# Optionally, include any additional commands or actions here
