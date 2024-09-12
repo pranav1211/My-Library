caeac49c20876c831f650b6052943433f7973311
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-09-12 13:31:13 +0530

    12/9

--- mylibgitman.js
+++ mylibgitman.js
@@ -1,13 +1,33 @@
 const express = require('express');
-const app = express();
+const crypto = require('crypto');
 const { exec } = require('child_process');
 const path = require('path');
+require('dotenv').config();
 
-// Absolute path to the shell script
 const scriptPath = '/shellfiles/test.sh';
+const GITHUB_SECRET = process.env.mylibgitkey || fs.readFileSync('/etc.environment','utf8').trim()
 
-// Middleware to parse JSON bodies
+const app = express();
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+    const signature = req.headers['x-hub-signature-256'];
+    const payload = JSON.stringify(req.body);
+
+    if (!signature) {
+        return res.status(401).json({ message: 'Signature required' });
+    }
+
+    const hmac = crypto.createHmac('sha256', GITHUB_SECRET);
+    const digest = `sha256=${hmac.update(payload).digest('hex')}`;
+
+    if (signature !== digest) {
+        return res.status(401).json({ message: 'Invalid signature' });
+    }
+
+    next();
+});
 
 app.post('/mylibg', (req, res) => {    
     exec(`sh ${scriptPath}`, (error, stdout, stderr) => {
@@ -16,13 +36,11 @@ app.post('/mylibg', (req, res) => {
             return res.status(500).json({ message: 'Error executing script', error: error.message });
         }
         
-        // Log the script output (for debugging purposes)
         console.log(`Script output: ${stdout}`);
         if (stderr) {
             console.error(`Script stderr: ${stderr}`);
         }
 
-        // Send the response
         res.status(200).json({ message: 'Script executed successfully', timestamp: new Date() });
     });
 });
