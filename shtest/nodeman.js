7e52132e42f71a376333f9e7c57ad057bfca9d66
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-09-12 12:08:52 +0530

    12/9

--- shtest/nodeman.js
+++ shtest/nodeman.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+const { exec } = require('child_process');
+
+
 app.get('/mylibg', (req, res) => {
-  res.status(200).json({ message: 'Hello, world!', timestamp: new Date() });
+    res.status(200).json({ message: 'Hello, world!', timestamp: new Date() });
+    exec('sh test.sh')
 });
 
 app.listen(6004, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
