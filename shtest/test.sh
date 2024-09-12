8473a7eaf6c592e1b7d70af6227241354619700b
Pranav Veeraghanta <theveeraghanta@gmail.com>
2024-09-12 12:35:43 +0530

    12/9

--- shtest/test.sh
+++ shtest/test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Define the log file path
-LOG_FILE="/gitlogs/myliblog.log"
+LOG_FILE="/path/to/logfile.log"
 
-# Get the current date and time
 CURRENT_TIME=$(date +"%Y-%m-%d %H:%M:%S")
 
-# Write a log entry
-echo "Script called at $CURRENT_TIME" >> "$LOG_FILE"
+cd ~/mylib
 
-# Optionally, include any additional commands or actions here
+sudo git pull
+
+echo "[$CURRENT_TIME] Pulled latest changes from Git repository" >> "$LOG_FILE"
