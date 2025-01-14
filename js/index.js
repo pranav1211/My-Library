c78cbfca30e89bce5c0b03692c71e20bbe060dfd
pranavisda1 <pranavisda1@gmail.com>
2025-01-15 00:08:48 +0530

    15/1

--- js/index.js
+++ js/index.js
@@ -3,3 +3,9 @@ var startit = document.querySelector('#startit')
 startit.addEventListener('click',()=>{
     window.location.href = "https://mylibrary.life/scanner.html";
 })
+
+var viewit = document.querySelector('#viewlib')
+
+viewit.addEventListener('click',()=>{
+    window.location.href = "https://mylibrary.life/yourlibrary.html";
+})
