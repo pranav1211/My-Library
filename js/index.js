4572385ae8c39943430aee8e706d790388a4e863
pranavisda1 <pranavisda1@gmail.com>
2025-02-18 16:00:58 +0530

    18/2

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 var startit = document.querySelector('#startit')
 
 startit.addEventListener('click',()=>{
-    window.location.href = "https://mylibrary.life/scanner.html";
+    window.location.href = "https://library.beyondmebtw.com/scanner.html";
 })
 
 var viewit = document.querySelector('#viewlib')
 
 viewit.addEventListener('click',()=>{
-    window.location.href = "https://mylibrary.life/yourlibrary.html";
+    window.location.href = "https://library.beyondmebtw.com/yourlibrary.html";
 })
